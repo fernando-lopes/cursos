7b5f1ccf9b48a7491f4473ef831471555434229f
fernandolopes@live.com <g#L0p35.1411>
2018-12-17 20:29:25 -0200

    Aulas avançadas.

---
+++ kotlin/src/avancado/Generico.kt
@@ -0,0 +1,27 @@
+package avancado
+
+class Caixa<T>(objeto: T) {
+    private val objetos = ArrayList<T>()
+    init {
+        adicionar(objeto)
+    }
+
+    fun adicionar(novoObjeto: T) {
+        objetos.add(novoObjeto)
+    }
+
+    override fun toString(): String {
+        return objetos.toString()
+    }
+}
+
+fun main(args: Array<String>) {
+    val materialEscolar = Caixa("Caneta")
+    materialEscolar.adicionar("Lapis")
+    materialEscolar.adicionar("Borracha")
+    println(materialEscolar)
+
+    val numeros = Caixa(objeto = 1)
+    numeros.adicionar(3)
+    println(numeros)
+}
